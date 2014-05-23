ffaf11ab24e0f0eb716fd158c11d3857015ec28b
neatpilot <neatpilot@users.noreply.github.com>
2014-05-23 00:50:15 -0300

    Used hash to represent mapping

--- homework_1/ruby-calisthenics/lib/rock_paper_scissors.rb
+++ homework_1/ruby-calisthenics/lib/rock_paper_scissors.rb
@@ -7,10 +7,9 @@ class RockPaperScissors
     raise NoSuchStrategyError.new("Strategy must be one of R,P,S") unless 
     	player1[1] =~ /[RPS]/ && player2[1] =~ /[RPS]/
 
-    s1 = represent_by_number(player1[1])
-    s2 = represent_by_number(player2[1])
+    refer = {'R' => 3, 'S' => 2, 'P' => 1}
 
-    if (s1 - s2 + 3 ) % 3 != 2
+    if (refer[player1[1]] - refer[player2[1]] + 3 ) % 3 != 2
     	player1
     else
     	player2
@@ -24,17 +23,6 @@ class RockPaperScissors
     else
     	winner(tournament_winner(tournament[0]),tournament_winner(tournament[1]))
     end
-  end
-
-  def self.represent_by_number(s)
-  	case s
-	  	when 'R'
-	  		3
-	  	when 'S'
-	  		2
-	  	when 'P'
-	  		1
-  	end
   end
 
 end
