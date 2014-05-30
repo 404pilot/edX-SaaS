b1aa344654d32da0b7102126c40b5b3247fecc93
neatpilot <neatpilot@users.noreply.github.com>
2014-05-30 20:54:07 -0300

    Finished HW02

--- homework_2/rails-intro/app/controllers/movies_controller.rb
+++ homework_2/rails-intro/app/controllers/movies_controller.rb
@@ -7,7 +7,37 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+
+    if !params[:ratings] or !params[:sortBy]
+      # missing parameters
+      ratings = params[:ratings]? params[:ratings] : session[:ratings]
+      ratings = ratings ? ratings : @all_ratings
+
+      sortBy = params[:sortBy]? params[:sortBy] : session[:sortBy]
+      sortBy = sortBy ? sortBy : ""
+
+      session[:ratings] = ratings
+      session[:sortBy] = sortBy
+
+      redirect_to movies_path(:ratings => session[:ratings], :sortBy => session[:sortBy])
+    end
+    
+    @sortBy = params[:sortBy]
+    @checked = params[:ratings].is_a?(Hash)? params[:ratings].keys : params[:ratings]
+
+    if @sortBy!="" && @checked
+      @movies = Movie.find(:all, :conditions => {rating: @checked}, :order => @sortBy)
+    elsif @checked
+      @movies = Movie.find(:all, :conditions => {rating: @checked})
+    elsif @sortBy != ""
+      @movies = Movie.find(:all, :order => @sortBy)
+    else
+      @movies = Movie.all
+    end
+
+    session[:ratings] = @checked
+    session[:sortBy] = @sortBy
   end
 
   def new
