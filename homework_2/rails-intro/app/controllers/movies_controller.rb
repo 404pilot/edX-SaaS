f15e121ec60895e1c4ae1799b6bf194ed99d3cc1
neatpilot <neatpilot@users.noreply.github.com>
2014-06-05 01:09:29 -0500

    Clean code

--- homework_2/rails-intro/app/controllers/movies_controller.rb
+++ homework_2/rails-intro/app/controllers/movies_controller.rb
@@ -11,14 +11,8 @@ class MoviesController < ApplicationController
 
     if !params[:ratings] or !params[:sortBy]
       # missing parameters
-      ratings = !!params[:ratings] ? params[:ratings] : session[:ratings]
-      ratings = !!ratings ? ratings : @all_ratings
-
-      sortBy = !!params[:sortBy] ? params[:sortBy] : session[:sortBy]
-      sortBy = !!sortBy ? sortBy : ""
-
-      session[:ratings] = ratings
-      session[:sortBy] = sortBy
+      session[:ratings] = params[:ratings] || session[:ratings] || @all_ratings
+      session[:sortBy] = params[:sortBy] || session[:sortBy] || ""
 
       redirect_to movies_path(:ratings => session[:ratings], :sortBy => session[:sortBy])
     end
