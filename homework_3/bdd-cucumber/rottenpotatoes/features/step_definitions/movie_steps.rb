5c6be3f7b0c1162a2fbeb54b9a3baa885bc26a6f
neatpilot <neatpilot@users.noreply.github.com>
2014-06-05 02:18:53 -0300

    Finished HW03_Part02

--- homework_3/bdd-cucumber/rottenpotatoes/features/step_definitions/movie_steps.rb
+++ homework_3/bdd-cucumber/rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,7 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    
+
     # movie here is a hash which is getting from web form
     Movie.create(title: movie['title'], rating: movie['rating'], release_date: movie['release_date'])
   end
@@ -27,10 +27,20 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  flunk "Unimplemented"
+  rating_list.split(",").each do |rating|
+    rating = rating.strip
+    rating_checkbox = "ratings_#{rating}"
+    if uncheck
+      uncheck(rating_checkbox)
+    else
+      check(rating_checkbox)
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  flunk "Unimplemented"
+  Movie.all.each do |movie|
+    page.should have_content(movie['title'])
+  end
 end
